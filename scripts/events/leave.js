d8731d391bbfc10a7ef827669ef232527c67e28d
Gohime Hatake <146865909+Elohime7@users.noreply.github.com>
2024-02-28 11:49:42 +0100

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -4,7 +4,7 @@ module.exports = {
 	config: {
 		name: "leave",
 		version: "1.4",
-		author: "NTKhang",
+		author: "NTKhang",//edited by Elohime Hatake
 		category: "events"
 	},
 
@@ -25,7 +25,7 @@ module.exports = {
 			session4: "evening",
 			leaveType1: "left",
 			leaveType2: "was kicked from",
-			defaultLeaveMessage: "{userName} {type} the group"
+			defaultLeaveMessage: "{userName} nous à quitter 😿"
 		}
 	},
 
